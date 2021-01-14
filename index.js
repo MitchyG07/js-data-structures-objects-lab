fc6ab9abd07ef925c5caf9ea3222cd756b57adb7
Mitch Gilbert <mitchellpgilbert@gmail.com>
2021-01-14 10:55:01 -0800

    commit tree a3481345044eab7c16fc9d0edae1ef8e5546ea69

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+// Write your solution in this file 
+function updateDriverWithKeyAndValue(obj, key, value) {
+    return Object.assign({}, obj, {[key]: value})
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(obj, key, value) {
+    return Object.assign(obj, {[key]: value})
+}
+
+function deleteFromDriverByKey(obj, key) {
+    let newObj = Object.assign({}, obj)
+    delete newObj[key];
+    return newObj
+}
+
+function destructivelyDeleteFromDriverByKey(obj, key) {
+    delete obj[key];
+    return obj
+}
+
+const driver = { car: 'ford'}
+const newDriver = destructivelyUpdateDriverWithKeyAndValue(driver,'car','Mercedes')
+
+const anotherDriver = deleteFromDriverByKey(driver, 'car')
